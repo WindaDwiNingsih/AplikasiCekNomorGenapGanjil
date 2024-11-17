4e20555d590280787033c2841f043747bd5c50c1
WindaDwiNingsih <dwiw48340@gmail.com>
2024-11-17 10:57:20 +0800

    cek ganjil genap

--- src/aplikasiganjilgenap/AplikasiGanjilGenapView.java
+++ src/aplikasiganjilgenap/AplikasiGanjilGenapView.java
@@ -70,6 +70,7 @@ public class AplikasiGanjilGenapView extends javax.swing.JFrame {
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jPanel1.setLayout(new java.awt.GridBagLayout());
 
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel3.setText("Masukkan Angka");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
@@ -79,6 +80,7 @@ public class AplikasiGanjilGenapView extends javax.swing.JFrame {
         gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 15);
         jPanel1.add(jLabel3, gridBagConstraints);
 
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel1.setText("Hasil");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
